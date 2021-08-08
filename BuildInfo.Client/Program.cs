2fd751c807602165377886f16e1151cb8fa64e64
Damir Arh <damir.arh@gmail.com>
2021-08-08 18:38:07 +0200

    Source generator and a sample project using it

---
+++ BuildInfo.Client/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BuildInfo.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Timestamp: {Build.Info.Timestamp}");
+            Console.WriteLine($"Git SHA: {Build.Info.GitSha}");
+        }
+    }
+}
---
+++ BuildInfo.Generator/BuildInfoGenerator.cs
@@ -0,0 +1,71 @@
+﻿using CliWrap;
+using Microsoft.CodeAnalysis;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildInfo.Generator
+{
+    [Generator]
+    public class BuildInfoGenerator : ISourceGenerator
+    {
+        private static readonly DiagnosticDescriptor MissingPropertyWarning = new DiagnosticDescriptor(
+            id: "BUILDINFOGEN001",
+            title: "Missing CompilerVisibleProperty",
+            messageFormat: "Generator requires MSBuildProjectDirectory CompilerVisibleProperty to get Git SHA",
+            category: "BuildInfoGenerator",
+            defaultSeverity: DiagnosticSeverity.Warning,
+            isEnabledByDefault: true);
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            if (!context.AnalyzerConfigOptions.GlobalOptions.TryGetValue($"build_property.MSBuildProjectDirectory", out var path))
+            {
+                context.ReportDiagnostic(Diagnostic.Create(MissingPropertyWarning, Location.None));
+            }
+
+            var buildTime = DateTime.Now;
+            var gitSha = this.GetGitSha(path).Result;
+
+            string source = $@"
+using System;
+
+namespace Build
+{{
+    public static class Info
+    {{
+        public static DateTime Timestamp {{ get; }} = new DateTime({buildTime.Ticks});
+        public static string GitSha {{ get; }} = ""{gitSha}"";
+    }}
+}}
+";
+            context.AddSource("BuildInfo", source);
+        }
+
+        private async Task<string> GetGitSha(string path)
+        {
+            var stdOutBuffer = new StringBuilder();
+            var stdErrBuffer = new StringBuilder();
+            try
+            {
+                await Cli.Wrap("git")
+                    .WithArguments("rev-parse --short HEAD")
+                    .WithWorkingDirectory(path)
+                    .WithStandardOutputPipe(PipeTarget.ToStringBuilder(stdOutBuffer))
+                    .WithStandardErrorPipe(PipeTarget.ToStringBuilder(stdErrBuffer))
+                    .ExecuteAsync();
+                return stdOutBuffer.ToString().Trim();
+            }
+            catch
+            {
+                return "N/A";
+            }
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+        }
+    }
+}
